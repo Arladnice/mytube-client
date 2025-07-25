18b0da29334d682cb7f29621c49ba9fe6774c3f0
Denis Arlakov <denarlakov12@gmail.com>
2025-07-25 15:59:44 +0300

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { COLORS } from './src/constants/colors.constants';
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: COLORS,
+    },
+  },
+  plugins: [],
+};
+
+export default config;
